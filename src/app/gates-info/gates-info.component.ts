1181bfb0864a070fda3e98669c0b1dc7b1f6db54
gravictor <56508699+gravictor@users.noreply.github.com>
2020-11-27 17:57:43 +0200

    after mentor check

--- src/app/gates-info/gates-info.component.ts
+++ src/app/gates-info/gates-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
+import { NgForm} from '@angular/forms';
 @Component({
   selector: 'app-gates-info',
   templateUrl: './gates-info.component.html',
@@ -10,7 +11,13 @@ export class GatesInfoComponent implements OnInit {
 
   responce: any;
   constructor(private http: HttpClient) { }
-
+  id= 1;
+  gate = "gate 111";
+  description = "last end";
+   
+  submit(form: NgForm){
+      console.log(form);
+  }
   show(){
     this.responce = this.http
     .get('http://localhost:8080/gates/hello2');
@@ -18,5 +25,8 @@ export class GatesInfoComponent implements OnInit {
   ngOnInit(): void {
     this.show();
   }
-
+  onSubmit(myForm: NgForm){
+     
+    console.log(myForm);
+}
 }

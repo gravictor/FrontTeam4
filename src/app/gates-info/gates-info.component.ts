44a7c8197e1338ae4a0a13b63b6185eae90b87df
gravictor <56508699+gravictor@users.noreply.github.com>
2020-11-29 22:20:48 +0200

    Post method

--- src/app/gates-info/gates-info.component.ts
+++ src/app/gates-info/gates-info.component.ts
@@ -2,25 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
 import { NgForm} from '@angular/forms';
+import { Gate } from '../Gate';
 @Component({
   selector: 'app-gates-info',
   templateUrl: './gates-info.component.html',
   styleUrls: ['./gates-info.component.scss']
 })
 export class GatesInfoComponent implements OnInit {
-
+  URL = 'http://localhost:8080/gates/';
+  gate: Gate = new Gate();
   responce: any;
   constructor(private http: HttpClient) { }
-  id= 1;
-  gate = "gate 111";
-  description = "last end";
-   
-  submit(form: NgForm){
-      console.log(form);
+  delete(id: number){
+    alert("Вы уверенны, что хотите удалить gate c ID " + id+" ?");
+  }
+  post(gat: Gate){
+    console.log()
+    const body = {name: this.gate.name, description: this.gate.description};
+    this.http.post<any>(this.URL+'hello3', body).subscribe(data => {
+      console.log(data)
+    })
   }
   show(){
     this.responce = this.http
-    .get('http://localhost:8080/gates/hello2');
+    .get(this.URL+'hello2');
   }
   ngOnInit(): void {
     this.show();
